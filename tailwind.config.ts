ce5ea83bb1355ee19496bd99923800e27f8b6de3
Dev-knowledge <utzsv1989@gmail.com>
2024-08-31 15:53:54 +0100

    second commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      extend: {
+        screens: {
+          'sm': '640px',
+          // => @media (min-width: 640px) { ... }
+    
+          'md': '768px',
+          // => @media (min-width: 768px) { ... }
+    
+          'lg': '1024px',
+          // => @media (min-width: 1024px) { ... }
+    
+          'xl': '1280px',
+          // => @media (min-width: 1280px) { ... }
+    
+          '2xl': '1440px',
+          // => @media (min-width: 1536px) { ... }
+        },
+        colors: {
+          'main': '#e002a2',
+          'second': '#47019d',
+          'three': '#e00256',
+          'black': '#212121',
+          'white': '#ffffff',
+          'gray': '#808080e2'
+        },
     },
   },
   plugins: [],
