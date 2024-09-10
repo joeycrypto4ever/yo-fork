7558ae48d30954b74121cf92ef4dfb5e719a7e60
Dev-knowledge <utzsv1989@gmail.com>
2024-09-10 19:12:29 +0100

    merging..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,32 @@
-<<<<<<< HEAD
 import Compagnies from "./components/compagnies"
+import Features from "./components/features"
 import Footer from "./components/Footer"
 import GetInTouch from "./components/Get-in-touch"
-import Header from "./components/Header"
 import OurProjects from "./components/our-projects"
 import Pricing from "./components/pricing"
 import Reviews from "./components/reviews"
-=======
 import React from 'react'
 import Header from './components/header'
 import MobileHeader from './components/mobile-header'
 import Hero from './components/hero'
-import Features from './components/features'
 import Process from './components/process'
->>>>>>> 772927c (all sections)
 
 function Home() {
   return (
-<<<<<<< HEAD
     <>
-    {/* <Header /> */}
-    <OurProjects />
-    <Reviews />
-    <Pricing />
-    <Compagnies />
-    <GetInTouch />
-    <Footer />
-    </>
-=======
-    <main>
       <Header />
       <MobileHeader />
       <Hero />
       <Features />
       <Process />
-    </main>
->>>>>>> 772927c (all sections)
+      <OurProjects />
+      <Reviews />
+      <Pricing />
+      <Compagnies />
+      <GetInTouch />
+      <Footer />
+    </>
+
   )
 }
 
